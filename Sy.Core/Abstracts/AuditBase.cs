7dcc122ce73eeaebf971e986584e61d953d338e6
Suat Üstünkaya <suatustunkaya@gmail.com>
2019-09-03 10:53:00 +0300

    ......

---
+++ Sy.Core/Abstracts/AuditBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sy.Core.Abstracts
+{
+    public abstract class AuditBase //  Bu sınıftan nesene oluşturmk istiyorum ama kalıtım almak istemiyorum
+    {
+
+        
+        [StringLength(50)]
+        public string CeratedUSer { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now; // Constructer yazmaya gerek kalmadan burada eşitleme yaptık
+
+        [StringLength(50)]
+        public string UpdatedUser { get; set; }
+        public DateTime? UpdateDate { get; set; } // Boş geçilemeyecği için ? işareti ekledik
+
+
+    }
+}
---
+++ Sy.Core/Abstracts/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//-----------------
+using System.ComponentModel.DataAnnotations; // Key için ekledik
+
+namespace Sy.Core.Abstracts
+{
+    public abstract class BaseEntity<TKey> : IEntity<TKey>
+    {
+        [Key]
+        public TKey Id { get; set; }
+    }
+}
