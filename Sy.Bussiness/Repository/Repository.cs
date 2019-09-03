3609339c1f7978e2efd3ded73b49e74a2bcc1062
Suat Üstünkaya <suatustunkaya@gmail.com>
2019-09-03 15:15:30 +0300

    son güncellemeler

---
+++ Sy.Bussiness/Repository/Repository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations; // AddOrUpdate için ekledik Uppdate içindeki
+
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Sy.Core.Abstracts;
+using Sy.DataAccesLayer;
+
+namespace Sy.Bussiness.Repository
+{
+    public class Repository<TEntity, TKey> : IRepository<TEntity, TKey> where TEntity : class, IEntity<TKey>
+    {
+        private readonly StockContext _context;
+        private readonly DbSet<TEntity> _table;
+
+
+        public int Delete(TEntity entity)
+        {
+            _table.Remove(entity);
+            return this.Save();
+        }
+
+        public TEntity GetById(TKey Id)
+        {
+            return _table.Find(Id);
+        }
+
+        public TKey Insert(TEntity entity)
+        {
+            _table.Add(entity);
+            this.Save();
+            return entity.Id;
+        }
+
+        public IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            return predicate == null ? _table : _table.Where(predicate);
+        }
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+
+        public int Update(TEntity entity)
+        {
+            _table.AddOrUpdate(entity);
+            return this.Save();
+        }
+    }
+}
--- Sy.Forms/Form1.cs
+++ Sy.Forms/Form1.cs
@@ -14,6 +14,7 @@ namespace Sy.Forms
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
